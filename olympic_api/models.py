bda292e0c455a5efe8b9a4476ef72679425875fc
Victor de Almeida Fonseca <victor@sema.castanhal.com>
2020-02-05 13:44:09 -0300

    Improve the populate_base.py. To able to run py/pyhton manager.py populate_base

--- olympic_api/models.py
+++ olympic_api/models.py
@@ -12,17 +12,17 @@ class TeamRegion(models.Model):
     """
     Class for store information about teams who have been to the Olympic Games
     """
-    noc = models.CharField(max_length=3, null=False, blank=False, unique=True,
+    NOC = models.CharField(max_length=3, null=False, blank=False, unique=True,
                            help_text="Field store the initials of National Olympic Committee.")
 
-    name = models.CharField(max_length=500, null=False, blank=False, help_text="Field store the team's name")
+    region = models.CharField(max_length=500, null=False, blank=False, help_text="Field store the team's name")
     notes = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
         db_table = "team_region"
 
     def __str__(self):
-        return self.name
+        return self.region
 
 
 
@@ -37,7 +37,7 @@ class Athlete(models.Model):
     age = models.IntegerField(null=False, blank=False, help_text="Field store the Athlete's age")
     height = models.IntegerField(help_text="Field store the Athlete's height in centimeters")
     weight = models.IntegerField(help_text="Field store the Athlete's weight in kilograms")
-    noc_team_region = models.ForeignKey(TeamRegion, null=False, blank=False, on_delete=models.PROTECT, to_field='noc',
+    noc_team_region = models.ForeignKey(TeamRegion, null=False, blank=False, on_delete=models.PROTECT, to_field='NOC',
                                     help_text="Field store foreign key refers to TeamRegion noc field")
 
     class Meta:
